6fcaaf13dcc5e6267684c80cb471906db5863d7d
Predaotor <ladomarshania621@gmail.com>
2025-03-16 19:14:11 +0100

    Update dokerfiles for deployment add diagram and entrypoint

---
+++ puddle/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+
+echo "Starting Gunicorn..."
+exec gunicorn --workers 3 --bind 0.0.0.0:8000 puddle.wsgi
