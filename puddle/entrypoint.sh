4d2ce2ae3f8bc90bf58f7d66e017248c6e5ca055
Predaotor <164819387+Predaotor@users.noreply.github.com>
2025-03-17 04:53:07 +0100

    Update entrypoint.sh

--- puddle/entrypoint.sh
+++ puddle/entrypoint.sh
@@ -2,15 +2,11 @@
 
 # Exit immediately if a command exits with a non-zero status
 set -e
-echo "Starting Cloud SQL Auth Proxy..."
-# Start Cloud SQL Auth Proxy in the background
-/cloud_sql_proxy -dir=/cloudsql -instances="cool-artwork-445504:europe-central2:postgre123" &
 
-sleep 5
 
 echo "Running migrations..."
 python manage.py migrate --noinput
 
 
 echo "Starting Gunicorn..."
-exec gunicorn --workers 3 --bind 0.0.0.0:${PORT:-10000} puddle.wsgi:application
+exec gunicorn --workers 3 --bind 0.0.0.0:${PORT:-4000} puddle.wsgi:application
